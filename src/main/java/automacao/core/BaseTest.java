caeba60d17a1f25c4f499a9c92d78308f2a9b2fd
Boiteux <frederico.boiteux@capgemini.com>
2021-07-13 11:19:49 -0300

    incluir data e hora no screenshot

--- src/main/java/automacao/core/BaseTest.java
+++ src/main/java/automacao/core/BaseTest.java
@@ -3,11 +3,12 @@ package automacao.core;
 import static automacao.core.DriverFactory.getDriver;
 import static automacao.core.DriverFactory.killDriver;
 import static automacao.core.DriverFactory.getDriver;
-
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -16,14 +17,18 @@ import org.junit.rules.TestName;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-
 import automacao.core.Propriedades;
 
 public class BaseTest {
 	
 	@Rule
 	public TestName testName = new TestName(); 
-
+	
+	private String getDateTime() {
+		DateFormat dateFormat = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
 
 	public static void acessarTela () {
 		getDriver().get(Propriedades.ambientes.toString());
@@ -41,6 +46,6 @@ public class BaseTest {
 		TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
 		File arquivo = takesScreenshot.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(arquivo, new File(
-				"target" + File.separator + "screenshot" + File.separator + testName.getMethodName() + ".jpg"));
+				"target" + File.separator + "screenshot" + File.separator + testName.getMethodName() + "_" + getDateTime() + ".jpg"));
 	}
 }
